2559ec0c1a7345d7b382bb809e55eced38a483d6
haki36 <yildirim.h@hotmail.com>
2026-04-08 17:11:41 +0200

    docs(api): add complete OpenAPI documentation for blog endpoints

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -3,10 +3,22 @@ import os
 
 from flask import Flask, jsonify, request, redirect, url_for
 from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
+SWAGGER_URL="/api/docs"  # (1) swagger endpoint e.g. HTTP://localhost:5002/api/docs
+API_URL="/static/masterblog.json" # (2) ensure you create this dir and file
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Masterblog API' # (3) You can change this if you like
+    }
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 POSTS_FILE = os.path.abspath(
     os.path.join(app.root_path, '..', 'data', 'posts.json')
